a7723b533b70d1490f09414bc06089134fed52d0
NgoAnhKhai <anhkhaichannel@gmail.com>
2024-09-23 14:18:59 +0700

    router

---
+++ src/data.js
@@ -0,0 +1,29 @@
+let invoices = [
+  {
+    number: 1,
+    name: "John Doe",
+    amount: 100.0,
+    date: "2022-01-01",
+  },
+  {
+    number: 2,
+    name: "Jane Doe",
+    amount: 200.0,
+    date: "2022-01-15",
+  },
+  {
+    number: 3,
+    name: "John Doe",
+    amount: 300.0,
+    date: "2022-02-01",
+  },
+  {
+    number: 4,
+    name: "Jane Doe",
+    amount: 300.0,
+    date: "2022-02-01",
+  },
+];
+export function getInvoices() {
+  return invoices;
+}
